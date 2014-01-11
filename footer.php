e6c2060c9812d8d5336efa25f952a3edb482e6a8
Mega-Mario <thetotalworm@gmail.com>
2014-01-11 12:31:34 +0100

    stapling some butter

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 	<tr>
 	<td style="text-align: left;">
 		<img src="<?php echo resourceLink('img/poweredbyblarg.png'); ?>" style="float: left; margin-right: 3px;">
-		Blargboard 1.1 &middot; by Mega-Mario<br />
+		Blargboard 1.1 &middot; by StapleButter<br />
 		Based off <a href="http://abxd.dirbaio.net/">ABXD</a> by Dirbaio &amp; co.<br />
 		<?php echo '<!-- Page rendered in '.sprintf('%.03f',microtime(true)-$starttime).' seconds (with '.$queries.' SQL queries and '.sprintf('%.03f',memory_get_usage() / 1024).'K of RAM) -->'; ?>
 	</td>
