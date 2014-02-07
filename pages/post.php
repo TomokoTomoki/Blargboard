46b8182ff4803089c1d7735f1af7481d14d6f820
Mega-Mario <thetotalworm@gmail.com>
2014-02-07 16:33:20 +0100

    Fix post.php leaking titles of staff threads. Also add (untested) support for linking to a post by time (for future 'go to first unread post' implementation).

--- pages/post.php
+++ pages/post.php
@@ -1,8 +1,18 @@
 <?php
 
+if (isset($_GET['id']))
+{
+	$pid = (int)$_GET['id'];
+	$rPost = Query("select date,thread from {posts} where id={0}", $pid);
+}
+else if (isset($_GET['tid']) && isset($_GET['time']))
+{
+	$rPost = Query("select date,thread from {posts} where thread={0} AND date>{1} ORDER BY DATE LIMIT 1",
+		$_GET['tid'], $_GET['time']);
+}
+else
+	Kill('blarg');
 
-$pid = (int)$_GET['id'];
-$rPost = Query("select * from {posts} where id={0}", $pid);
 if(NumRows($rPost))
 	$post = Fetch($rPost);
 else
@@ -10,17 +20,19 @@ else
 
 $tid = $post['thread'];
 
-$rThread = Query("select * from {threads} where id={0}", $tid);
+$rThread = Query("select id,title,forum from {threads} where id={0}", $tid);
 
 if(NumRows($rThread))
 	$thread = Fetch($rThread);
 else
 	Kill(__("Unknown thread ID."));
+	
+$tags = ParseThreadTags($thread['title']);
 
 $ppp = $loguser['postsperpage'];
 if(!$ppp) $ppp = 20;
 $from = (floor(FetchResult("SELECT COUNT(*) FROM {posts} WHERE thread={1} AND date<={2} AND id!={0}", $pid, $tid, $post['date']) / $ppp)) * $ppp;
-$url = actionLink("thread", $thread["id"], $from?"from=$from":"", $thread["title"])."#post".$pid;
+$url = actionLink("thread", $thread['id'], $from?"from=$from":"", HasPermission('forum.viewforum', $thread['forum'], true)?$tags[0]:'')."#post".$pid;
 
 header("HTTP/1.1 301 Moved Permanently");
 header("Status: 301 Moved Permanently");
